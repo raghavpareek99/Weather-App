202f7c6d61460b98bb9cddd5dee0d97f2735c72c
Raghav Pareek <136429442+raghavpareek99@users.noreply.github.com>
2024-05-20 11:26:02 +0530

    Update App.js

--- App.js
+++ App.js
@@ -38,7 +38,7 @@ export default class App extends React.Component{
   }
 
   fetch_weather=()=> {
-    fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02')
+    fetch('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=1dd68d8ac6a9b13e512550abc5ce0ad4')
       .then((response) => response.json())
       .then((json) => {
         this.setState({ data: json });
